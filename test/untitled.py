83a432784c56148deb4f8ca39b3288d46c088084
bin曹斌 <binjacobcao@gmail.com>
2024-12-20 10:37:54 +0800

    remove

--- test/untitled.py
+++
@@ -1,7 +0,0 @@
-import sys
-import pandas as pd
-Yourdir = '/Users/jacob/Documents/GitHub/'
-sys.path.append(Yourdir)
-from PyWPEM import WPEM
-
-latt, AtomCoordinates = WPEM.CIFpreprocess(filepath='Mn2O3.cif',two_theta_range=(20, 80),show_unitcell=True)
